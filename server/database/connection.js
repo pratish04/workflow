883e632070404e5756397bd46697f193626b3e66
Pratish Barthwal <pratishbarthwal@gmail.com>
2023-04-23 21:57:49 +0530

    added some files

---
+++ server/database/connection.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mysql = require('mysql');
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+        host: 'bn5al3w0zspico0ivrt5-mysql.services.clever-cloud.com',
+        user: 'ufjyfc29cnzsnhvt',
+        password: process.env.DATABASE_PASSWORD,    
+        database: "bn5al3w0zspico0ivrt5",
+        multipleStatements: true,
+    });
+    
+module.exports=connection;
